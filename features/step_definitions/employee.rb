992dc4270adbb29bb7eb8d676a966e01d90d1313
Nicks-Souza <nicolas.souza990@gmail.com>
2025-07-31 16:12:44 -0300

    adicionando requisição put

--- features/step_definitions/employee.rb
+++ features/step_definitions/employee.rb
@@ -37,3 +37,28 @@ expect(@create_employee.parsed_responde['data']["employee_name"]).to eql 'Maravi
 expect(@create_employee.parsed_responde['data']["employee_salary"]).to eql (12600)
 expect(@create_employee.parsed_responde['data']["employee_age"]).to eql (25)
 end
+
+
+Dado('que o usuario altere uma informacao de funcionario') do
+@get_employee = HTTParty.get('http://dummy.restapiexample.com/api/v1/employees', :headers => {'Content-Type': 'application/json'})
+@put_url = 'https://dummy.restapiexample.com/api/v1/update/' + @get_employee['data'][0]['id'].to_s
+end
+
+Quando('ele enviar as novas informacoes') do
+ @update_employee = HTTParty.put(@put_url, :headers => {'Content-Type': 'application/json'}, body: {
+            "employee_name": "Maravilha",
+            "employee_salary": 100,
+            "employee_age": 25,
+            "profile_image": ""
+ }.to_json)
+end
+
+Entao('as informacoes serao alteradas') do
+    expect(@update_employee.code).to eql (200)
+    expect(@update_employee.message).to eql 'OK'
+    expect(@update_employee["status"]).to eql 'success'
+    expect(@update_employee["message"]).to eql 'Successfully! Record has been updated.'
+    expect(@update_employee['data']["employee_name"]).to eql 'Maravilha'
+    expect(@update_employee['data']["employee_salary"]).to eql (100)
+    expect(@update_employee['data']["employee_age"]).to eql (25)
+end
